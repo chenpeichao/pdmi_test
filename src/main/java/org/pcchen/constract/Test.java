85706e13ae6ea7878ae01dfe1d1a5c34e13d373f
chenpeichao <chenpeichao@pdmi.cn>
2018-01-23 08:38:40 +0800

    自动生成uuid

---
+++ src/main/java/org/pcchen/constract/Test.java
@@ -0,0 +1,11 @@
+package org.pcchen.constract;
+
+public class Test {
+	public static void main(String[] args) {
+		User user = new User(null, null);
+		System.out.println(user);
+		
+		String phoneNum= "17602987789";
+		System.out.println(phoneNum.substring(7));
+	}
+}
---
+++ src/main/java/org/pcchen/uuid/TestUUID.java
@@ -0,0 +1,10 @@
+package org.pcchen.uuid;
+
+import java.util.UUID;
+
+public class TestUUID {
+	public static void main(String[] args) {
+		//70f364da-27b4-40a8-8a25-96017d523d46
+		System.out.println(UUID.randomUUID().toString());
+	}
+}
